f9a852dd305e0f7e35e390f44becb75c3a916a45
Evan Florentino <evanflorentino122020@gmail.com>
2026-09-19 00:18:37 -0400

    Use database URL environment variable

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine, String
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "postgresql+psycopg://jobtracker:jobtracker@db:5432/job_tracker"
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 
